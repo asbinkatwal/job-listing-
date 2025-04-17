521a1b5c59c7c58515552d78f3754d0cfda53c42
Asbin Katwal <katwalasbin79@gmail.com>
2025-04-17 17:43:51 +0545

    add database pgadmin

--- joblisting/settings.py
+++ joblisting/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'joblisting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'job_listings',
+        'USER': 'postgres',
+        'PASSWORD': 'Asbin123',
+        'HOST': 'localhost',  
+        'PORT': '5432',        
     }
 }
+
 
 
 # Password validation
